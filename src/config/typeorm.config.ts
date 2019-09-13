32ed6323242ccd1f7c7381b53a18d4cc7839eba4
Joe Previte <joe@echobind.com>
2019-09-13 11:24:23 -0700

    fix: try other way for env vars

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,11 +6,11 @@ const dbConfig = config.get('db');
 console.log('SHOW ME THE dbConFIG', dbConfig);
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: dbConfig.host,
-  port: dbConfig.port,
-  username: dbConfig.username,
-  password: dbConfig.password,
-  database: dbConfig.database,
+  host: process.env.HOST || dbConfig.host,
+  port: process.env.PORT || dbConfig.port,
+  username: process.env.DATABASE_USERNAME || dbConfig.username,
+  password: process.env.DATABASE_PASSWORD || dbConfig.password,
+  database: process.env.DATABASE_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: dbConfig.synchronize,
 };
